d2cbec951a639fbf7181f1be4b3d7f16d7dfd1f2
Luuuh <luciana@kazap.com.br>
2022-06-26 13:04:43 -0300

    Fix img issue from Subscribe Page

--- src/pages/Subscribe.tsx
+++ src/pages/Subscribe.tsx
@@ -1,8 +1,8 @@
-import { gql, useMutation } from "@apollo/client";
 import { useState, FormEvent } from "react";
 import { useNavigate } from "react-router-dom";
 import Logo from "../components/Logo";
 import { useCreateSubscriberMutation } from '../graphql/generated';
+import imgUrl from '../../src/assets/code-mockup.png'
 
 export function Subscribe() {
   const navigate = useNavigate()
@@ -66,9 +66,8 @@ export function Subscribe() {
             </form>
           </div>
         </div>
-      
 
-      <img src="/src/assets/code-mockup.png" className="mt-10" alt=""/>
+      <img src={imgUrl} className="mt-10" alt="" />
     </div>
   )
 }
